e542f46dd811649342034d9b74b230cd2b46a70c
dariusz <dariusz.gierejkiewicz@bit-sa.pl>
2017-09-21 14:32:28 +0200

    sample image data

--- controllers/home.js
+++ controllers/home.js
@@ -6,6 +6,29 @@
 module.exports = {
     index: function (req, res) {
         // res.send('The home:index controller');
-        res.render('index');
+        // res.render('index');
+
+        var viewModel = {
+            images: [{
+                    uniqueId: 1,
+                    title: 'Sample Image 1',
+                    description: '',
+                    filename: 'sample1.jpg',
+                    views: 0,
+                    likes: 0,
+                    timestamp: Date.now
+                },
+                {
+                    uniqueId: 2,
+                    title: 'Sample Image 2',
+                    description: '',
+                    filename: 'sample2.jpg',
+                    views: 0,
+                    likes: 0,
+                    timestamp: Date.now
+                }
+            ]
+        };
+        res.render('index', viewModel);
     }
 };
